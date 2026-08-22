24a94aabef92d93cd0eab638f1112c7d41af62b9
bellanzo <abimbolaomisakin678@gmail.com>
2026-08-22 02:50:36 +0100

    refactor: register entities and migrations explicitly in data source

--- backend/src/infrastructure/database/data-source.ts
+++ backend/src/infrastructure/database/data-source.ts
@@ -1,13 +1,22 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { SessionEntity } from '../../modules/auth/entities/session.entity';
+import { UserEntity } from '../../modules/users/entities/user.entity';
+import { CreateUsersTable1787144227927 } from '../../../migrations/1787144227927-CreateUsersTable';
+import { CreateSessionsTableAndAddedNameToUserEntityDateTables1787151177780 } from '../../../migrations/1787151177780-CreateSessionsTableAndAddedNameToUserEntityDateTables';
+import { FixEmailVerifiedAtColumnTypo1787152000000 } from '../../../migrations/1787152000000-FixEmailVerifiedAtColumnTypo';
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/../../../migrations/*{.ts,.js}'],
+  entities: [UserEntity, SessionEntity],
+  migrations: [
+    CreateUsersTable1787144227927,
+    CreateSessionsTableAndAddedNameToUserEntityDateTables1787151177780,
+    FixEmailVerifiedAtColumnTypo1787152000000,
+  ],
   synchronize: false,
   ssl: false,
 });
